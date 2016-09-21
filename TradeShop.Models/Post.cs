3583034272206c8068ee755aac2dd6f51c218a97
Benjamin Ørskov <ben.oe@icloud.com>
2016-09-21 22:40:54 +0200

    init push

---
+++ TradeShop.Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeShop.Models
+{
+   public class Post
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public string[] Pictures { get; set; }
+        public int fuck { get; set; }
+    }
+}
